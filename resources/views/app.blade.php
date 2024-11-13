e623fa129cd7b8d2c7ca91fc2d4de8775ed840d1
trevor <t.f.schuil@gmail.com>
2024-11-13 02:17:02 +0000

    fb

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,6 +14,28 @@
         @routes
         @vite(['resources/scss/app.scss', 'resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
+    
+        <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+            appId      : '875441981418396',
+            cookie     : true,
+            xfbml      : true,
+            version    : '21'
+            });
+            
+            FB.AppEvents.logPageView();   
+            
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+        </script>
     </head>
     <body class="font-sans antialiased">
         @inertia
