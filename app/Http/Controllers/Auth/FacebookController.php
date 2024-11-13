f8e14534499efd66735c606532afc8ccde0ffe74
trevor <t.f.schuil@gmail.com>
2024-11-13 06:33:03 +0000

    wip

--- app/Http/Controllers/Auth/FacebookController.php
+++ app/Http/Controllers/Auth/FacebookController.php
@@ -17,23 +17,43 @@ class FacebookController extends Controller
     public function handleFacebookCallback()
     {
         try {
-            $user = Socialite::driver('facebook')->user();
-            $finduser = User::where('facebook_id', $user->id)->first();
+            // $user = Socialite::driver('facebook')->user();
+            // $finduser = User::where('facebook_id', $user->id)->first();
 
-            if ($finduser) {
-                //Auth::login($finduser);
-                //return redirect()->intended('dashboard');
-            } else {
-                $newUser = User::create([
-                    'name' => $user->name,
-                    'email' => $user->email,
-                    'facebook_id'=> $user->id,
-                    'password' => encrypt('123456dummy')
-                ]);
+            // if ($finduser) {
+            //     //Auth::login($finduser);
+            //     //return redirect()->intended('dashboard');
+            // } else {
+            //     $newUser = User::create([
+            //         'name' => $user->name,
+            //         'email' => $user->email,
+            //         'facebook_id'=> $user->id,
+            //         'password' => encrypt('123456dummy')
+            //     ]);
 
-                //Auth::login($newUser);
-                //return redirect()->intended('dashboard');
-            }
+            //     //Auth::login($newUser);
+            //     //return redirect()->intended('dashboard');
+            // }
+
+            $user = Socialite::driver('github')->user();
+ 
+            // OAuth 2.0 providers...
+            $token = $user->token;
+            $refreshToken = $user->refreshToken;
+            $expiresIn = $user->expiresIn;
+        
+            // OAuth 1.0 providers...
+            $token = $user->token;
+            $tokenSecret = $user->tokenSecret;
+        
+            // All providers...
+            $user->getId();
+            $user->getNickname();
+            $user->getName();
+            $user->getEmail();
+            $user->getAvatar();
+
+            echo "tok:" . $token . " sec:" . $tokenSecret;
         } catch (\Exception $e) {
             dd($e->getMessage());
         }
