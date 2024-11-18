943796633d702e5f677568f05779a1ea78bd8a28
trevor <t.f.schuil@gmail.com>
2024-11-18 06:50:32 +0000

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    css: {
+        preprocessorOptions: {
+          scss: {
+            api: 'modern-compiler' // or "modern"
+          }
+        }
+    },
+      
     plugins: [
         laravel({
             input: ['resources/js/app.js','resources/scss/app.scss',],
