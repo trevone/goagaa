cbf2df46366cc956fc62d0795bcc96b54edf8ba2
trevor <t.f.schuil@gmail.com>
2024-11-20 07:36:21 +0000

    schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('app:dispatch-job-chains')->everyThirtyMinutes();
+        $schedule->command('app:dispatch-job-chains')->everySixHours(rand(0, 59));
     }
 
     /**
